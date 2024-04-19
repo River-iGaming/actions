22724ca0f1b80765a41a4660f1602dce83994650
jonathansant <jonathan.sant@river.tech>
2024-04-19 13:59:26 +0200

    sp

--- dotnet/tag-check/index.js
+++ dotnet/tag-check/index.js
@@ -1,6 +1,7 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-const { execSync } = require('child_process');
+const { execSync } = require("child_process");
+const semver = require("semver");
 
 try {
     const branch = github.context.ref.replace("refs/heads/", "");
@@ -25,27 +26,36 @@ try {
         throw `Tag v${version} already exists`;
     }
 
-    const isReleaseBranch  = branch.startsWith("release");
+    const isReleaseBranch = branch.startsWith("release");
     const coreVersionTagMatch = tags.find(x => x.indexOf(packageVersion) > -1);
     const lastComment = Object.values(github.context.payload.commits).sort((a, b) => a.timestamp < b.timestamp ? 0 : -1)[0]?.message;
-    
+
     console.log(Object.values(github.context.payload.commits).sort((a, b) => a.timestamp < b.timestamp ? 0 : -1));
 
     console.log(`Last comment: ${lastComment}`);
     console.log(`Core version tag matched: ${coreVersionTagMatch}`);
 
-    if (isVersionAlteredInNonReleaseBranch(isReleaseBranch, coreVersionTagMatch) && !isReleaseBranchMerge(lastComment)) {
+    const isReleaseBranchMerge = isReleaseBranchMerge(lastComment);
+    if (isVersionAlteredInNonReleaseBranch(isReleaseBranch, coreVersionTagMatch) && !isReleaseBranchMerge) {
         throw `Version was altered in a non release branch`;
+    }
+
+    if(isReleaseBranchMerge) {
+        console.log(`Release branch merge detected`);
+
+        if(exactVersionTag && semver.gt(version, exactVersionTag.replace("refs/tags/", ""))) {
+            throw `Version is smaller than the previous version`;
+        }
     }
 
 } catch (error) {
     core.setFailed(error);
 }
 
-function isVersionAlteredInNonReleaseBranch(isReleaseBranch, coreVersionTagMatch){
+function isVersionAlteredInNonReleaseBranch(isReleaseBranch, coreVersionTagMatch) {
     return !isReleaseBranch && !coreVersionTagMatch;
 }
 
-function isReleaseBranchMerge(lastComment){
+function isReleaseBranchMerge(lastComment) {
     return lastComment?.indexOf(`chore(*): merge release/MW-`) > -1;
 }
