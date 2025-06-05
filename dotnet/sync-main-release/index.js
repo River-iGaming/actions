0c254aeb7e4de169f9a9c9cef13116134d990046
jonathansant <jonathan.sant@river.tech>
2025-06-05 18:07:02 +0200

    sp

--- dotnet/sync-main-release/index.js
+++ dotnet/sync-main-release/index.js
@@ -3,8 +3,10 @@ const github = require("@actions/github");
 const { execSync } = require("child_process");
 
 try {
-	const branch = github.context.ref.replace("refs/heads/", "");
+	// const branch = github.context.ref.replace("refs/heads/", "");
 	const runNumber = github.context.runNumber;
+	const releaseBranch = github.core.getInput("release-branch", { required: true });
+
 
 	console.log(`Branch: ${branch}`);
 
@@ -17,6 +19,14 @@ try {
 		.filter(x => x)
 		;
 
+	execSync(`git checkout ${releaseBranch}`);
+	console.log(`Checked out to release branch: ${releaseBranch}`);
+
+	execSync(`git merge origin/master --no-ff -m "chore(*): merge master into ${{ releaseBranch }}`);
+	console.log(`Merged master into release branch: ${releaseBranch}`);
+
+	execSync(`git push origin ${releaseBranch}`);
+	console.log(`Pushed changes to remote release branch: ${releaseBranch}`);
 
 	console.log("✅ All checks passed successfully!");
 } catch (error) {
